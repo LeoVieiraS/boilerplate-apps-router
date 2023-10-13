92bef814bea30b59d3e2f5c8c5935dd95aa487cc
leonardo.vieira <leonardo.vieira@mercafacil.com>
2023-10-13 17:51:26 -0300

    Add Styled components

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,11 @@ module.exports = {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.ts(x)?'],
+  collectCoverageFrom: [
+    'src/**/*.ts(x)?',
+    '!src/app/**', // should be tested in 2e2
+    '!src/lib/registry.tsx'
+  ],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
   modulePaths: ['<rootDir>/src/'],
   transform: {
